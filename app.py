be6839d99555c9c1764436bd05e7f0f371ebe8c2
Michael McCune <msm@redhat.com>
2017-06-14 13:46:03 -0400

    convert argparse to environment variables
    This fits better with the s2i model for the default python stuff.

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-import argparse
+import os
 from kafka import KafkaConsumer
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--servers', help='the kafka brokers')
-    parser.add_argument('--topic', help='the kafka topic to read')
-    args = parser.parse_args()
-
-    consumer = KafkaConsumer(args.topic, bootstrap_servers=args.servers)
+    topic = os.environ('KAFKA_TOPIC')
+    servers = os.environ('KAFKA_SERVERS')
+    consumer = KafkaConsumer(topic, bootstrap_servers=servers)
     for msg in consumer:
         print(msg)
 
